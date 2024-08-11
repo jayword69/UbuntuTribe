c266f9b871ad28e4575a938f5718da48b64b669f
blendInIt <135830356+jayword69@users.noreply.github.com>
2024-08-11 07:54:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// scripts.js
+
+// Efecto de Scroll Suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Animaciones al Hacer Scroll
+const animatedElements = document.querySelectorAll('.animate-on-scroll');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+}, { threshold: 0.5 });
+
+animatedElements.forEach(element => observer.observe(element));
